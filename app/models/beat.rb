dd1dc755030c511e5e9d0c6a10141e0c499bd441
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-07-01 18:53:48 -0400

    add selection for price

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -1,12 +1,14 @@
 class Beat < ActiveRecord::Base
 
   GENRE = ['Old School', 'Pop', 'Alternative', 'Reggae', 'Underground', 'Jazzy', 'R&B']
+  PRICE = [0.00, 0.99]
 
   attr_accessible :beat, :genre, :name, :price
 
   validates :beat, :genre, :name, :price, presence: true
 
   validates :genre, inclusion: { in: GENRE }
+  validates :price, inclusion: { in: PRICE }
 
   has_attached_file :beat,
           path: "beats/:id.:extension"
--- spec/models/beat_spec.rb
+++ spec/models/beat_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe Beat, focus: true do
   GENRE = ['Old School', 'Pop', 'Alternative', 'Reggae', 'Underground', 'Jazzy', 'R&B']
+  PRICE = [0.00, 0.99]
 
   it { should validate_presence_of(:beat) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:genre) }
   it { should validate_presence_of(:name) }
   it { should ensure_inclusion_of(:genre).in_array(GENRE)}
+  it { should ensure_inclusion_of(:price).in_array(PRICE)}
 
 end
