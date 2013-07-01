36236ab20522a21e93ca3160606fee3067f01b9e
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-07-01 18:51:25 -0400

    add selection for genre

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -1,7 +1,12 @@
 class Beat < ActiveRecord::Base
+
+  GENRE = ['Old School', 'Pop', 'Alternative', 'Reggae', 'Underground', 'Jazzy', 'R&B']
+
   attr_accessible :beat, :genre, :name, :price
 
   validates :beat, :genre, :name, :price, presence: true
+
+  validates :genre, inclusion: { in: GENRE }
 
   has_attached_file :beat,
           path: "beats/:id.:extension"
--- spec/models/beat_spec.rb
+++ spec/models/beat_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
-describe Beat do
+describe Beat, focus: true do
+  GENRE = ['Old School', 'Pop', 'Alternative', 'Reggae', 'Underground', 'Jazzy', 'R&B']
 
   it { should validate_presence_of(:beat) }
   it { should validate_presence_of(:price) }
   it { should validate_presence_of(:genre) }
   it { should validate_presence_of(:name) }
+  it { should ensure_inclusion_of(:genre).in_array(GENRE)}
 
 end
