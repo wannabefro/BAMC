d7fbd64873e60ebcb1d5dbc1ec261dab9ee3c05c
acacheung <acacheung@gmail.com>
2013-07-02 19:25:24 -0400

    better admin beats page

--- spec/features/beat_admin_spec.rb
+++ spec/features/beat_admin_spec.rb
@@ -8,17 +8,19 @@ feature 'adding beats as an admin' do
   scenario 'admin can add a new beat' do
     admin_beats
     expect(page).to have_content('Add a new beat')
+    click_link 'Add a new beat'
+    expect(current_path).to eql(new_admin_beat_path)
   end
 
-  scenario "if not an admin you can't visit the beats page" do
+  scenario 'user does not have link to add a new beat' do
     sign_in_as user
     expect(page).to_not have_content('Add a new beat')
   end
 
-  scenario 'an admin can add beats from the beats page' do
-    admin_beats
-    click_on 'Add a new beat'
-    expect(current_path).to eql(new_admin_beat_path)
+  scenario 'user cannot go to new admin beats path' do
+    sign_in_as user
+    visit new_admin_beat_path
+    expect(page).to have_content('You are not authorized!')
   end
 
   def admin_beats
