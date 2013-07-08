0adb99fca8ed9c5cddcdf5618d1c341c46fb021d
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-07-08 16:29:22 -0400

    Modify beats_conrtoller.rb

--- app/controllers/api/version1/beats_controller.rb
+++ app/controllers/api/version1/beats_controller.rb
@@ -1,12 +1,14 @@
 module Api
   module Version1
     class BeatsController<ApplicationController
+
       def index
         @beats = Beat.all
         respond_to do |format|
           format.json {render json: @beats}
         end
       end
+
     end
   end
 end
