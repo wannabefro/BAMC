83ccfe2e0189b977e16b0e3a1ba8e63bd3157fa7
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-07-08 17:24:11 -0400

    Add nested tracks to user_serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,18 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :url
+  attributes :username, :url, :tracks
 
   def url
     "http:www.bamc.co/mc/#{username}"
+  end
+
+  def tracks
+    output = []
+    object.tracks.each do |track|
+      if track.state == "public"
+        output << track
+      end
+    end
+    output
   end
 
 end
