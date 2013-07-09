9601888dec1a60db31e1ac99e0dba2ee320e0447
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-07-09 15:21:21 -0400

    change genres

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -1,7 +1,7 @@
 class Beat < ActiveRecord::Base
   require 'securerandom'
 
-  GENRE = ['Old School', 'Pop', 'Trap', 'Alternative', 'Grime', 'Hip Hop', 'R&B']
+  GENRE = ['Old School', 'Pop', 'Trap', 'Alternative', 'Electronic', 'Grime', 'Hip Hop', 'R&B']
   PRICE = [0.00, 0.99]
   STATE = ['approve', 'pending', 'reject']
 
