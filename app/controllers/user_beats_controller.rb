ba3a4fd1cf423a6600a29a00fd6d76fdb5a68451
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-07-17 17:43:08 -0400

    Modify user_beats_controller.rb

--- app/controllers/user_beats_controller.rb
+++ app/controllers/user_beats_controller.rb
@@ -1,7 +1,37 @@
 class UserBeatsController < ApplicationController
   
-  def new
+  def new 
     @user_beat = UserBeat.new
   end
+
+  def destroy
+    @user_beat = UserBeat.find(params[:id])
+    if @user_beat.destroy
+      redirect_to dashboard_index_path(current_user), notice: 'Successfully deleted your original beat'
+    else
+      redirect_to dashboard_index_path(current_user)
+    end
+ 
+  end
+
+  def download
+    @user_beat = UserBeat.find(params[:id])
+    data = open(@user_beat.user_beat)
+    send_data data.read, type: 'audio/wav', filename: @user_beat.name, x_sendfile: true
+  end
+
+  def public
+    @user_beats = UserBeat.find(params[:id])
+    @user_beats.publicize
+    redirect_to :back
+  end
+
+  def private
+    @user_beat = UserBeat.find(params[:id])
+    @user_beat.privatize
+    redirect_to :back
+  end
+
+
 
 end
